129b676ff15f8b823b59b3f4c5a24a71380da941
Leonardo Brito <lbrito@gmail.com>
2019-03-18 11:47:50 -0300

    Fixes count of vovo terms.

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ var regular_clickbaits = ["choca " + city, "vira febre em " + city]
 var clickbaits = []
 
 for (var vovo in granny_clickbaits) {
-  if (page_text.includes()) granny_index += 100;
+  if (page_text.includes(vovo)) granny_index += 100;
+}
+for (var regular in regular_clickbaits) {
+  if (page_text.includes(regular)) granny_index += 10;
 }
 
 console.log("This page's Granny Index is "+granny_index);
